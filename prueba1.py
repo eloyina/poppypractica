537c8996535dbeaee67d4108469952fcc926b8d4
Elizabeth Morales Muñoz <elizabeth.morales@ucsp.edu.pe>
2022-09-21 23:14:42 -0500

    Update prueba1.py
    Agregando funciones

--- prueba1.py
+++ prueba1.py
@@ -1,10 +1,85 @@
 import time
 from matplotlib import pyplot as plt
-
+import itertools
 import matplotlib.pyplot as plot 
 from pypot.creatures import PoppyHumanoid
 poppy = PoppyHumanoid(simulator='vrep')
 
+def run():
+        poppy.goto_position({'l_hip_y': -39,
+                                  'r_hip_y': -39,
+                                  'l_knee_y': 134,
+                                  'r_knee_y': 134,
+                                  'abs_x': 0,
+                                  'abs_y': 3,
+                                  'abs_z': 0,
+                                  'bust_x': 0,
+                                  'bust_y': 0,
+                                  },
+                                 2,
+                                 wait=True)
+
+
+        for m in poppy.motors:
+            m.compliant = True
+
+        for m in poppy.motors:
+            m.torque_limit = 20
+
+        time.sleep(2)
+
+
+
+def run2():
+    poppy.goto_position(dict(zip((m.name for m in poppy.motors), itertools.repeat(0))), 2, wait=True)
+
+        # Specified some motor positions to keep the robot balanced
+    poppy.goto_position({'r_hip_z': -2,
+                                  'l_hip_z': 2,
+                                  'r_hip_x': -2,
+                                  'l_hip_x': 2,
+                                  'l_shoulder_x': 20,
+                                  'r_shoulder_x': -20,
+                                  'l_shoulder_y': 10,
+                                  'r_shoulder_y': 10,
+                                  'l_elbow_y': -20,
+                                  'r_elbow_y': -20,
+                                  'l_ankle_y': -3,
+                                  'r_ankle_y': -2,
+                                  'abs_y': -2,
+                                  'head_y': 1,
+                                  'head_z': 0},
+                                 3,
+                                 wait=True)
+
+        # Restore the motor speed
+    poppy.power_up()
+
+        # Reduce max torque to keep motor temperature low
+    for m in poppy.motors:
+        m.torque_limit = 70
+
+    time.sleep(1)
+
+def setup():
+    poppy.compliant = False
+    poppy.power_up()
+
+        # Change PID of Dynamixel MX motors
+    for m in [m for m in poppy.motors if hasattr(m, 'pid')]:
+        m.pid = (4, 2, 0)
+    for m in poppy.torso:
+        m.pid = (6, 2, 0)
+
+        # Reduce max torque to keep motor temperature low
+    for m in poppy.motors:
+        m.torque_limit = 70
+
+    time.sleep(0.5)
+
+    def run(self):
+        pass
+    
 def Saludar():
     for m in poppy.motors:
         m.moving_speed = 40 #velocidad del motor
@@ -91,9 +166,13 @@ def motores():
 #task_caminar=caminar()
 #motores()
 #alias()
-test()
+#test()
 """
 inicial()
 """
+
+setup()
+#run()
+run2()
 #Mover = move()
 #Mover
